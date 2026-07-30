8d1590d273e8702192077495abc8d2253f4f544a
김민서 <kimminseo@gmail.com>
2026-07-30 16:40:26 +0900

    feat: 협업 유형 검사 이탈 확인 모달 추가

--- src/app/collaboration-type/questions/[questionId]/page.tsx
+++ src/app/collaboration-type/questions/[questionId]/page.tsx
@@ -1,8 +1,6 @@
 import { notFound } from "next/navigation";
 
-import TeamMatchingHeader from "@/components/team-matching/TeamMatchingHeader";
-
-import CollaborationQuestionForm from "../../_components/CollaborationQuestionForm";
+import CollaborationQuestionPageContent from "../../_components/CollaborationQuestionPageContent";
 import {
   COLLABORATION_TEST_QUESTIONS,
   COLLABORATION_TEST_TOTAL_QUESTION_COUNT,
@@ -38,32 +36,15 @@ export default async function CollaborationTypeQuestionPage({
 
   return (
     <main className="relative flex h-full w-full flex-col overflow-hidden bg-[#F9F8F4] text-[#1F1F1F]">
-      <TeamMatchingHeader
-        backHref="/collaboration-type"
-        className="bg-[#F9F8F4]"
-        title="협업 유형 검사"
+      <CollaborationQuestionPageContent
+        currentQuestionId={currentQuestionId}
+        nextHref={nextHref}
+        options={question.options}
+        previousHref={previousHref}
+        progressWidth={progressWidth}
+        title={question.title}
+        totalQuestionCount={COLLABORATION_TEST_TOTAL_QUESTION_COUNT}
       />
-
-      <div className="relative mx-auto mt-[34px] flex h-[6px] w-[322px] max-w-[calc(100%-32px)] flex-col items-start gap-[10px] rounded-[90px] bg-[#D9D9D9]">
-        <div
-          className="h-[6px] shrink-0 rounded-[90px] bg-[#FFAD62]"
-          style={{ width: `${progressWidth}px` }}
-        />
-        <p className="absolute bottom-[-16px] right-0 text-right font-[Pretendard] text-[12px] font-semibold leading-[135%]">
-          <span className="text-[#FF7658]">{currentQuestionId}</span>
-          <span className="text-[#C8C8C8]">/{COLLABORATION_TEST_TOTAL_QUESTION_COUNT}</span>
-        </p>
-      </div>
-
-      <section className="mt-[36px] flex flex-col px-[18px]">
-        <CollaborationQuestionForm
-          currentQuestionId={currentQuestionId}
-          nextHref={nextHref}
-          options={question.options}
-          previousHref={previousHref}
-          title={question.title}
-        />
-      </section>
     </main>
   );
 }
