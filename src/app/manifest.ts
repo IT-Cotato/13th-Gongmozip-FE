d7d2c41087912db98c4c4d4b4dd86067775e7157
김민서 <kimminseo@gmail.com>
2026-08-18 17:03:05 +0900

    fix: PWA 192x192 아이콘 추가

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,6 +13,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ff7658",
     icons: [
       {
+        src: "/images/logo-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
         src: "/images/logo.png",
         sizes: "512x512",
         type: "image/png",
